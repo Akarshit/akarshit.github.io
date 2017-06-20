26de0f89951a1c9e914746944d6a7b0382e5d902
Akarshit <akarshitwal@gmail.com>
2017-06-20 22:25:41 +0530

    5 done

--- redis.js
+++ redis.js
@@ -1,5 +1,5 @@
 document.write(`
-<div id="myModal3" class="modal fade" role="dialog">
+<div id="myModal4" class="modal fade" role="dialog">
   <div class="modal-dialog">
 
     <!-- Modal content-->
@@ -10,17 +10,18 @@ document.write(`
       </div>
       <div class="modal-body">
       <h2> The Problem </h2>
-      The server handling the socket connection was not able to scale as it had to handle a lot of socket connections simultaneously.
+      To make the queries on databases superfast.
       <h2> The Solution </h2>
-      The server was split into two servers </br>
-      1. This server handles the socket messages and just puts them in a RabbitMQ "request" queue and listen to the "reply" queue for responses. </br>
-      2. This server's task is to listen to the "request" queue, pick up messages from the queue, process them and send them back to the reply queue. </br>
+      If the data was only stored in MongoDB, the query time is in ms. This is OK for a individual calls but in a game this time is not acceptable.
+      So the data was split between MongoDB and Redis. When a game is live all the data associated with it is kept in Redis and when the game is over, the data is moved from Redis to MongoDB.
       <h3>Architecture</h3>
-      <img src="images/redisArchi.png">
+      <img src="images/redisArchi.jpg">
       <h3> Explanation </h3> 
-      The socket server is a simple NodeJS application written using socket.io. Beacuse of using RabbitMQ the socket server is now free of all the computation. It's only job is to maintain connection with the client and interact with RabbitMQ.
-      All the heavy load lifting is being done by the workers.
-      This helped up to build a scalable architecture.
+      Redis is a database which keeps all the data in memory. This causes some superfast response times. A get query gets executed in order of ns.</br>
+      During the game the socket server is reponsible for all the communication with the client. As during the game all the data is stored in Redis, the Socket server only had to communicate with Redis which gives very less response time. </br>
+      After the game has finished, the data is transferred from Redia to MongoDB. Now when this data has to be queried in the future, the request will come to API server and will be addresses by MongoDB.
+      To make redis always available, a cluster of redis insatances is deployed. Each mater has 2 slaves.
+      To make redis faster, sharding is done between 2 masters.
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
